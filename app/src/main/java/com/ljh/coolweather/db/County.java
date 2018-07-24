04bc745aee8515eede234262b9edfb5c05017f83
ljh <1246007250@qq.com>
2018-07-24 16:34:59 +0800

    加入显示天气信息功能。

--- app/src/main/java/com/ljh/coolweather/db/County.java
+++ app/src/main/java/com/ljh/coolweather/db/County.java
@@ -4,7 +4,7 @@ import org.litepal.crud.DataSupport;
 
 public class County extends DataSupport {
     private int id;
-    private String coutyName;
+    private String countyName;
     private String weatherId;
     private int cityId;
 
@@ -16,12 +16,12 @@ public class County extends DataSupport {
         this.id = id;
     }
 
-    public String getCoutyName() {
-        return coutyName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCoutyName(String coutyName) {
-        this.coutyName = coutyName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
     public String getWeatherId() {
